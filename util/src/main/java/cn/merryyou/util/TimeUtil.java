0a0191b1d7adafc2b31ff932928e06fe673e947f
longfeizheng <longfei_zheng@163.com>
2017-03-03 10:04:12 +0800

    添加util模块

---
+++ util/src/main/java/cn/merryyou/util/TimeUtil.java
@@ -0,0 +1,31 @@
+package cn.merryyou.util;
+
+import java.util.TimeZone;
+
+/**
+ * Created on 2017/3/3 0003.
+ *
+ * @author zlf
+ * @since 1.0
+ */
+public class TimeUtil {
+    public static final int SECONDS_IN_DAY = 60 * 60 * 24;
+    public static final long MILLIS_IN_DAY = 1000L * SECONDS_IN_DAY;
+
+    /**
+     * 判断两个long型的毫秒是不是同一天
+     * @param ms1
+     * @param ms2
+     * @return
+     */
+    public static boolean isSameDayOfMillis(final long ms1, final long ms2) {
+        final long interval = ms1 - ms2;
+        return interval < MILLIS_IN_DAY
+                && interval > -1L * MILLIS_IN_DAY
+                && toDay(ms1) == toDay(ms2);
+    }
+
+    private static long toDay(long millis) {
+        return (millis + TimeZone.getDefault().getOffset(millis)) / MILLIS_IN_DAY;
+    }
+}
